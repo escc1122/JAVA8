590ac6e0a4f6be79fea7cb07d4873a953308dfe3
escc1122 <escc1122@gmail.com>
2020-11-23 17:44:33 +0800

    Add files via upload

--- stream_test/src/main/java/com/example/Test2.java
+++ stream_test/src/main/java/com/example/Test2.java
@@ -13,12 +13,20 @@ public class Test2 {
         a.add(4);
         a.add(5);
         a.add(6);
-        String b = String.join(",",a.stream().map(String::valueOf).collect(Collectors.toList()));
+        String result1 = String.join(",",a.stream().map(String::valueOf).collect(Collectors.toList()));
 
-        System.out.println(b);
+        System.out.println("result1 => " + result1);
 
         String result2 =  a.stream().map(x -> x.toString()).collect(Collectors.joining(","));
-        System.out.println(result2);
+        System.out.println("result2 => " + result2);
+
+
+        String result3 =  a.stream().filter(x -> x< 3).map(x -> x.toString()).collect(Collectors.joining(","));
+        System.out.println("result3 => " + result3);
+
+//        result1 => 1,2,3,4,5,6
+//        result2 => 1,2,3,4,5,6
+//        result3 => 1,2
 
     }
 }
